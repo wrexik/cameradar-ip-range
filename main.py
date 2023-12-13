d736d1f0805ce36a63847ac13a69b71ce79a9779
wrexik <hofinfik@gmail.com>
2023-12-13 14:36:14 +0100

    oops

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def run_container(target_ip):
 
         # Loop to print output while the container is running
         for line in docker_process.stdout:
-            if not line.find("no stream found")
+            if not line.find("no stream found"):
                 print("OK - Streams found")
                 found_in_ip = +1
             
